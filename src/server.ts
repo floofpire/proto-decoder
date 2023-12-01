05502bd95f7c102c67eb2431d75dadcabcd77ab5
Naji Astier <naji.astier@algoan.com>
2023-12-01 22:55:23 +0100

    refactor: migrate server to Elysia

--- src/server.ts
+++ src/server.ts
@@ -1,53 +1,52 @@
+import { Elysia, t } from 'elysia';
+
 import { decodeDownMessage, decodeUpMessage } from './decoder.ts';
 import { saveMessage } from './file-persistor.ts';
 import { runMigrations, getDbClient } from './db/client.ts';
 import { saveMessageInDatabase } from './dbPersistor.ts';
 import { logger } from './logger.ts';
 
-interface RequestBody {
-  message?: string;
-}
-
 const port = 29323;
 
 await getDbClient();
 await runMigrations();
 
-Bun.serve({
-  port,
-  async fetch(request) {
-    const url = new URL(request.url);
-    if (!request.body) {
-      return new Response('Missing request body', { status: 400 });
-    }
+const app = new Elysia()
+  .model({
+    proto: t.Object({
+      message: t.String(),
+    }),
+  })
+  .onRequest(({ request }) => {
+    logger.silly(`Received request: ${request.url}`);
+  })
+  .post(
+    '/down',
+    ({ body }) => {
+      const decodedMessage = decodeDownMessage(body.message);
+      saveMessage(`${decodedMessage.reply_seq}-down-${decodedMessage.reply_svr_ts}`, decodedMessage);
+      saveMessageInDatabase(decodedMessage);
 
-    const body = await request.json<RequestBody>();
-    if (!body.message) {
-      return new Response('Missing body message', { status: 400 });
-    }
+      logger.info(`Received ${decodedMessage.reply_seq}-down message: ${body.message.length}`);
+      return 'OK';
+    },
+    {
+      body: 'proto',
+    },
+  )
+  .post(
+    '/up',
+    ({ body }) => {
+      const decodedMessage = decodeUpMessage(body.message);
+      saveMessage(`${decodedMessage.seq}-up-${decodedMessage.sign}`, decodedMessage);
 
-    switch (url.pathname) {
-      case '/down': {
-        const decodedMessage = decodeDownMessage(body.message);
-        saveMessage(`${decodedMessage.reply_seq}-down-${decodedMessage.reply_svr_ts}`, decodedMessage);
-        saveMessageInDatabase(decodedMessage);
+      logger.info(`Received ${decodedMessage.seq}-up message: ${body.message.length}`);
+      return 'OK';
+    },
+    {
+      body: 'proto',
+    },
+  )
+  .listen(port);
 
-        logger.info(`Received ${decodedMessage.reply_seq}-down message: ${body.message.length}`);
-
-        return new Response('OK', { status: 201 });
-      }
-      case '/up': {
-        const decodedMessage = decodeUpMessage(body.message);
-        saveMessage(`${decodedMessage.seq}-up-${decodedMessage.sign}`, decodedMessage);
-
-        logger.info(`Received ${decodedMessage.seq}-up message: ${body.message.length}`);
-
-        return new Response('OK', { status: 201 });
-      }
-      default:
-        return new Response('Not found', { status: 404 });
-    }
-  },
-});
-
-logger.info(`Server started on http://localhost:${port}`);
+logger.info(`Server started on http://localhost:${app.server?.port}`);
