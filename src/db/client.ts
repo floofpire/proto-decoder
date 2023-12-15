2409a861209be814c966d4ab00b82e2e45744706
Naji Astier <na-ji@users.noreply.github.com>
2023-12-15 21:50:42 +0100

    refactor(db): use connection pool instead of client

--- src/db/client.ts
+++ src/db/client.ts
@@ -19,15 +19,20 @@ export const getDbClient = async () => {
     return db;
   }
 
-  const connection = await mysql.createConnection({
+  const pool = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     port: Number(process.env.DATABASE_PORT),
+    waitForConnections: true,
+    connectionLimit: 1,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
   });
 
-  db = drizzle(connection, { logger: drizzleLogger });
+  db = drizzle(pool, { logger: drizzleLogger });
 
   return db;
 };
